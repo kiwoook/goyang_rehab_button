4c41f6e92400d72e10e98e4b32009d9ba9740693
kiwoook <tizmfns1218@Naver.com>
2024-10-02 15:35:09 +0900

    Fix: 위치 조정

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -68,8 +68,8 @@ const Survey = () => {
           display: "flex",
           width: "100vw",
           height: "100vh", // 화면 전체를 차지하게 함
+          paddingTop:"5rem",
           justifyContent: "center",
-          alignItems: "center", // 수직 가운데 정렬
           flexDirection: "column",
         }}
       >
@@ -155,7 +155,7 @@ const Survey = () => {
                   <svg viewBox="0 0 24 24">
                     <path d="M12,17.5C14.33,17.5 16.3,16.04 17.11,14H6.89C7.69,16.04 9.67,17.5 12,17.5M8.5,11A1.5,1.5 0 0,0 10,9.5A1.5,1.5 0 0,0 8.5,8A1.5,1.5 0 0,0 7,9.5A1.5,1.5 0 0,0 8.5,11M15.5,11A1.5,1.5 0 0,0 17,9.5A1.5,1.5 0 0,0 15.5,8A1.5,1.5 0 0,0 14,9.5A1.5,1.5 0 0,0 15.5,11M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
                   </svg>
-                  <span className="rating-text">불만족</span>
+                  <span className="rating-text">매우 만족</span>
                 </label>
                 <label htmlFor="happy" className="rating-label">
                   <input
@@ -242,7 +242,7 @@ const Survey = () => {
                 type="submit"
                 style={{ height: "10vh", width: "10vw" }}
               >
-                전송
+                <span style={{fontSize:"2.5rem"}}>전송</span>
               </ModalButton>
             </div>
           </Container>
